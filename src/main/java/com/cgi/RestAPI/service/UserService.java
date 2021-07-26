0ef0a118e0cd52f4ae27b3bace718185423ac92c
Nikhil Kumar <nikhil.kumar1@cgi.com>
2021-07-26 15:30:24 +0530

    changes saved

--- src/main/java/com/cgi/RestAPI/service/UserService.java
+++ src/main/java/com/cgi/RestAPI/service/UserService.java
@@ -7,9 +7,14 @@ import java.util.List;
 public interface UserService {
 
     User saveUser (User user);
+
     List<User> getAllUsers();
+
     void deleteUser(int id);
+
     List<User> nameSearch(String name);
+
     List<User> ageSearch(int age);
+
     List<User> genderSearch(String gender);
 }
